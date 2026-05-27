0e6cb72ecc9251b27e58bd13b5952d5c569d8808
Vanessa Ivonne Rodriguez Esquivel <vanessaesquivel867@gmail.com>
2026-05-27 15:47:22 -0600

    Se actualiza contenido

--- Administrativos Nivel A/Horarios Alumnos.php
+++ Administrativos Nivel A/Horarios Alumnos.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "Horarios Alumnos";
+echo "<h1>CRUD Horarios Profesores</h1>";
+
+$horarios = [
+    [
+        "profesor" => "Juan Pérez",
+        "materia" => "Programación",
+        "hora" => "08:00 AM"
+    ],
+    [
+        "profesor" => "Ana López",
+        "materia" => "Matemáticas",
+        "hora" => "10:00 AM"
+    ]
+];
+
+echo "<h3>Horarios registrados</h3>";
+
+foreach($horarios as $h){
+
+    echo "Profesor: " . $h['profesor'] . "<br>";
+    echo "Materia: " . $h['materia'] . "<br>";
+    echo "Hora: " . $h['hora'] . "<br><br>";
+}
 
 ?>
