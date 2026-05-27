0dc5132e7665c3f05a37fe007b11d019008a6857
Vanessa Esquivel <vanessaesquivel867@gmail.com>
2026-05-27 14:37:17 -0600

    Update CRUD Materias.php
    Se actualiza contenido

--- Administrativos Nivel A/CRUD Materias.php
+++ Administrativos Nivel A/CRUD Materias.php
@@ -1,5 +1,20 @@
 <?php
 
-echo "Horarios Profesores";
+echo "<h1>CRUD Materias</h1>";
 
+$materias = [
+    "Matemáticas",
+    "Programación",
+    "Base de Datos"
+];
+
+echo "<h3>Lista de materias</h3>";
+
+foreach($materias as $materia){
+    echo $materia . "<br>";
+}
+
+echo "<hr>";
+
+echo "Materia agregada correctamente";
 ?>
